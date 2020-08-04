6ecad0269c1605ef5f8f270f4dbf1c39bfe0e2d6
britindia <63962202+britindia@users.noreply.github.com>
2020-08-04 16:59:28 -0700

    Add files via upload

--- trainer/save_model.py
+++ trainer/save_model.py
@@ -1,7 +1,16 @@
 #! /usr/bin/env python
 import tensorflow as tf
+import os
 
-meta_path = 'gs://ordinal-reason-282519-aiplatform/text_cnn_training_071320201841/checkpoints/model-22000.meta' # Your .meta file
+# Params
+tf.flags.DEFINE_string("bucket",'', "Google Storage Bucket location")
+tf.flags.DEFINE_string("checkpoint_path",'', "Google Storage Bucket output folder")
+tf.flags.DEFINE_string("meta_name",'', "latest meta graph name")
+
+FLAGS = tf.flags.FLAGS
+
+# meta_path = 'gs://ordinal-reason-282519-aiplatform/text_cnn_training_071320201841/checkpoints/model-22000.meta' # Your .meta file
+meta_path = os.path.join(FLAGS.bucket,FLAGS.checkpoint_path,FLAGS.meta_name) # Your .meta file
 output_node_names = ['output/predictions']    # Output nodes
 
 with tf.Session() as sess:
@@ -9,16 +18,17 @@ with tf.Session() as sess:
     saver = tf.train.import_meta_graph(meta_path)
 
     # Load weights
-    saver.restore(sess,tf.train.latest_checkpoint('gs://ordinal-reason-282519-aiplatform/text_cnn_training_071320201841/checkpoints/'))
+#     saver.restore(sess,tf.train.latest_checkpoint('gs://ordinal-reason-282519-aiplatform/text_cnn_training_071320201841/checkpoints/'))
+    saver.restore(sess,tf.train.latest_checkpoint(os.path.join(FLAGS.bucket,FLAGS.checkpoint_path)))
 
     # Freeze the graph
 #     output_node_names = [n.name for n in tf.get_default_graph().as_graph_def().node]
-    print("output_node_names_1 : {}".format(output_node_names))
+    print("output_node_names : {}".format(output_node_names))
     frozen_graph_def = tf.graph_util.convert_variables_to_constants(
         sess,
         sess.graph_def,
         output_node_names)
 
     # Save the frozen graph
-    with open('text_cnn3.pb', 'wb') as f:
+    with open('text_cnn_2020.pb', 'wb') as f:
       f.write(frozen_graph_def.SerializeToString())
